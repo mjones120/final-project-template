2227468a6919fca41932d967a93fb1c00a9b6a76
mjones <you@example.com>
2024-04-16 12:23:22 -0400

    'main'

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,7 +9,7 @@ class MainController extends Controller
 
     public function homepage()
     {
-                include '../public/assets/views/main/homepage.php';
+                include '../public/assets/views/main/homepage.html';
 //        $this->view('../public/assets/views/main/example-homepage.php', true);
     }
 
--- public/index.php
+++ public/index.php
@@ -6,11 +6,21 @@ $env = parse_ini_file('../.env');
 require '../app/core/config.php';
 
 use app\core\Router;
+use app\controllers\MainController;
+use app\controllers\UserController;
 
 require_once __DIR__ . '/helpers.php';
 
 //to add a new route add to the app/core/routes.php array
 $router = new Router($routes);
 $router->serveRoute();
+
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+
+if($uri === '/homepage' && $_SERVER['REQUEST_METHOD']=== 'GET')
+{
+    $controller = new MainController();
+    $controller->homepage();
+}
 ?>
 
