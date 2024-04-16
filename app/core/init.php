d41465a4776c781d0572ad6ea1f50a013cf4f83d
mjones <you@example.com>
2024-04-16 15:13:54 -0400

    'main'

--- app/core/init.php
+++ app/core/init.php
@@ -1,5 +1,8 @@
 <?php
 define('APP_ROOT', dirname(__FILE__, 3));
+define('APP_PUBLIC', dirname(__FILE__, 3) . '/public');
+define('APP_APP', dirname(__FILE__, 3) . '/app');
+define('APP_VIEWS', dirname(__FILE__, 3) . '/public/assets/views');
 
 spl_autoload_register(function ($classname) {
     require $filename = APP_ROOT . '/' . str_replace('\\', '/', $classname) . ".php";
