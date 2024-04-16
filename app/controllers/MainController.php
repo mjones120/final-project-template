f6e1504d02e60f4f9f060bd6d8d5b88aad267696
mjones <you@example.com>
2024-04-16 15:15:38 -0400

    'main'

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,30 +10,30 @@ class MainController extends Controller
     public function homepage()
     {
                 include '../public/assets/views/main/homepage.html';
-                $this->view('../public/assets/views/main/example-homepage.html', true);
+                $this->view('../public/assets/views/main/homepage.html', true);
 //        $this->view('../public/assets/views/main/example-homepage.php', true);
     }
     public function certs()
     {
         include '../public/assets/views/main/certs.html';
-        $this->view('../public/assets/views/main/example-certs.html', true);
+        $this->view('../public/assets/views/main/certs.html', true);
     }
     public function projects()
     {
         include '../public/assets/views/main/projects.html';
-        $this->view('../public/assets/views/main/example-projects.html', true);
+        $this->view('../public/assets/views/main/projects.html', true);
     }
     public function contact()
     {
         include '../public/assets/views/main/contact.html';
-        $this->view('../public/assets/views/main/example-contact.html', true);
+        $this->view('../public/assets/views/main/contact.html', true);
     }
     public function learn()
     {
         include '../public/assets/views/main/learn.html';
-        $this->view('../public/assets/views/main/example-learn.html', true);
+        $this->view('../public/assets/views/main/learn.html', true);
     }
-    
+
 
     public function notFound()
     {
