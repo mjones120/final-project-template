e66886de0f68e4bd2b54e8f538c868ad383664ce
mjones <you@example.com>
2024-04-16 13:35:16 -0400

    'main'

--- app/core/routes.php
+++ app/core/routes.php
@@ -11,5 +11,25 @@ $routes = [
     'view-users' => [
         'controller' => UserController::class,
         'GET' => 'getUsers',
+    ],
+    'homepage' => [
+        'controller' => MainController::class,
+        'GET' => 'homepage'
+    ],
+    'certs' => [
+        'controller' => MainController::class,
+        'GET' => 'certs'
+    ],
+    'projects' => [
+        'controller' => MainController::class,
+        'GET' => 'projects'
+    ],
+    'contact' => [
+        'controller' => MainController::class,
+        'GET' => 'contact'
+    ],
+    'learn' => [
+        'controller' => MainController::class,
+        'GET' => 'learn'
     ]
 ];
