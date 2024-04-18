74143df936ec42b19ef11218c758aee6f4ab1fc5
mjones <you@example.com>
2024-04-18 12:37:44 -0400

    'main'

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace app\controllers;
-
 use app\core\Controller;
 
 class MainController extends Controller
--- app/models/projects.php
+++ app/models/projects.php
@@ -4,7 +4,6 @@ namespace app\models;
 
 use app\core\Database;
 
-
 class Projects
 {
     use Database;
